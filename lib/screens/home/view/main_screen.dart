d907389ffa69ff65caa71c730fc5566967a34018
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-02-07 21:54:08 +0530

    Added credit card

--- lib/screens/home/view/main_screen.dart
+++ lib/screens/home/view/main_screen.dart
@@ -51,7 +51,110 @@ class MainScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              const Icon(CupertinoIcons.settings)
+              IconButton(
+                  onPressed: () {}, icon: const Icon(CupertinoIcons.settings))
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width / 2,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              gradient: LinearGradient(colors: [
+                Theme.of(context).colorScheme.secondary,
+                Theme.of(context).colorScheme.tertiary,
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Total balance",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 12),
+              const Text("Rs. 300,000",
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          width: 20,
+                          height: 20,
+                          decoration: const BoxDecoration(
+                              color: Colors.white30, shape: BoxShape.circle),
+                          child: const Center(
+                            child: Icon(
+                              CupertinoIcons.arrow_up,
+                              size: 12,
+                              color: Colors.green,
+                            ),
+                          )),
+                      const SizedBox(width: 8),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Income",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400)),
+                          Text("Rs. 250,000",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600))
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                          width: 20,
+                          height: 20,
+                          decoration: const BoxDecoration(
+                              color: Colors.white30, shape: BoxShape.circle),
+                          child: const Center(
+                            child: Icon(
+                              CupertinoIcons.arrow_down,
+                              size: 12,
+                              color: Colors.red,
+                            ),
+                          )),
+                      const SizedBox(width: 8),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Expenses",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400)),
+                          Text("Rs. 100,000",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600))
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              )
             ],
           ),
         )
